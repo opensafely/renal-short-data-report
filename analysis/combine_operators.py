159a32a3bad7c59811e80a513df9deaddee0dcb7
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-03-17 16:49:37 +0000

    remove duplicate

--- analysis/combine_operators.py
+++ analysis/combine_operators.py
@@ -5,12 +5,11 @@ from pathlib import Path
 
 OUTPUT_DIR = Path("output")
 
-def drop_and_round(column):
-    column[column<=5] = 0
-    return column.apply(lambda x: 5 * round(float(x)/5))
 
-def custom_round(x, base=5):
-    return int(base * round(float(x)/base))
+def drop_and_round(column):
+    column[column <= 5] = 0
+    return column.apply(lambda x: 5 * round(float(x) / 5))
+
 
 def match_input_files(file: str) -> bool:
     """Checks if file name has format outputted by cohort extractor"""
@@ -34,6 +33,7 @@ def combine_value_with_operator(df, value_column, operator_column):
         df[value_column].astype("str")
     )
 
+
 # counts of each numeric value - operator pair
 value_counts_creatinine = []
 value_counts_cr_cl = []
@@ -47,20 +47,25 @@ operators_creatinine = []
 operators_cr_cl = []
 
 
-
-
 for file in OUTPUT_DIR.iterdir():
     if match_input_files(file.name):
         df = pd.read_csv(OUTPUT_DIR / file.name)
         date = get_date_input_file(file.name)
 
         # how many numeric values have a matched operator?
-        num_with_numeric_value_and_operator_creatinine = df.loc[df["creatinine_numeric_value"].notnull(),:].groupby("creatinine_operator")[["creatinine"]].sum()
+        num_with_numeric_value_and_operator_creatinine = (
+            df.loc[df["creatinine_numeric_value"].notnull(), :]
+            .groupby("creatinine_operator")[["creatinine"]]
+            .sum()
+        )
         operators_creatinine.append(num_with_numeric_value_and_operator_creatinine)
 
-        num_with_numeric_value_and_operator_cr_cl = df.loc[df["cr_cl_numeric_value"].notnull(),:].groupby("cr_cl_operator")[["cr_cl"]].sum()
+        num_with_numeric_value_and_operator_cr_cl = (
+            df.loc[df["cr_cl_numeric_value"].notnull(), :]
+            .groupby("cr_cl_operator")[["cr_cl"]]
+            .sum()
+        )
         operators_cr_cl.append(num_with_numeric_value_and_operator_cr_cl)
-
 
         combine_value_with_operator(
             df, "creatinine_numeric_value", "creatinine_operator"
@@ -110,7 +115,9 @@ creatinine_codes_count.to_csv(OUTPUT_DIR / "creatinine_codes_count.csv")
 
 
 creatinine_operators = pd.concat(operators_creatinine)
-creatinine_operators_count = creatinine_operators.groupby(creatinine_operators.index).sum()
+creatinine_operators_count = creatinine_operators.groupby(
+    creatinine_operators.index
+).sum()
 creatinine_operators["creatinine"] = drop_and_round(creatinine_operators["creatinine"])
 creatinine_operators_count.to_csv(OUTPUT_DIR / "creatinine_operators_count.csv")
 
