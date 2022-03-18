e32d5d197baedcc37380f72e7c83150799870237
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-03-18 17:22:15 +0000

    make codelist naming consistent

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -5,19 +5,19 @@ from cohortextractor import (
 )
 
 
-eGFR_tests_codes = codelist_from_csv(
+eGFR_codelist = codelist_from_csv(
     "codelists/user-ss808-estimated-glomerular-filtration-rate-egfr-recorded-tests.csv",
     system="snomed",
     column="code"
 )
 
-eGFR_values_codes = codelist_from_csv(
+eGFR_level_codelist = codelist_from_csv(
     "codelists/user-ss808-estimated-glomerular-filtration-rate-egfr-values.csv", 
     system="snomed", 
     column="code",
 )
 
-ethnicity_codes=codelist_from_csv(
+ethnicity_codelist = codelist_from_csv(
     "codelists/primis-covid19-vacc-uptake-eth2001.csv",
     system="snomed",
     column="code",
