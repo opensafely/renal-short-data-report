4d9494f248e8bedfb196ef240e0c39cec6125ac8
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-04-01 16:21:28 +0100

    use numpy instead of pandas

--- analysis/plot_numeric_values.py
+++ analysis/plot_numeric_values.py
@@ -15,13 +15,13 @@ for file in (OUTPUT_DIR / "joined").iterdir():
     if match_input_files(file.name):
         df = pd.read_csv((OUTPUT_DIR / "joined") / file.name)
         date = get_date_input_file(file.name)
-        numeric_values_creatinine.append(df["creatinine_numeric_value"])
-        numeric_values_cr_cl.append(df["cr_cl_numeric_value"])
-        numeric_values_egfr.append(df["eGFR_numeric_value"])
+        numeric_values_creatinine.append(np.array(df["creatinine_numeric_value"]))
+        numeric_values_cr_cl.append(np.array(df["cr_cl_numeric_value"]))
+        numeric_values_egfr.append(np.array(df["eGFR_numeric_value"]))
 
-numeric_values_creatinine = pd.concat(numeric_values_creatinine)
-numeric_values_cr_cl = pd.concat(numeric_values_cr_cl)
-numeric_values_egfr = pd.concat(numeric_values_egfr)
+numeric_values_creatinine = np.concatenate(numeric_values_creatinine)
+numeric_values_cr_cl = np.concatenate(numeric_values_cr_cl)
+numeric_values_egfr = np.concatenate(numeric_values_egfr)
 
 def plot_hist_numeric_value(x, title, filename):
     counts, bins, bars = plt.hist(x, bins=50)
@@ -30,6 +30,7 @@ def plot_hist_numeric_value(x, title, filename):
     plt.ylabel("count")
     plt.xlabel("numeric_value")
     plt.savefig(f'output/{filename}.jpeg')
+    plt.clf()
     
 
 plot_hist_numeric_value(numeric_values_creatinine, "Creatinine numeric value distribution", "creatinine_dist")
