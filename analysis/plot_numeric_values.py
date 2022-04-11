d8aca14ab19d87744f34887eb834d3944991672a
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-04-11 11:39:36 +0100

    replace hist with boxplot

--- analysis/plot_numeric_values.py
+++ analysis/plot_numeric_values.py
@@ -29,18 +29,16 @@ def plot_boxplot_numeric_value(x, title, filename):
     plt.clf()
 
 
-def plot_hist_numeric_value(x, title, filename):
-    counts, bins, bars = plt.hist(x, bins=10)
-    frequency_counts = [int(x) for x in counts]
-    pd.Series(frequency_counts, name="Counts").to_csv(
-        f"output/{filename}_frequency.csv"
-    )
-    plt.ylabel("count")
-    plt.xlabel("numeric_value")
-    plt.savefig(f"output/{filename}.jpeg")
-    plt.clf()
-
-
-plot_hist_numeric_value(numeric_values_creatinine, "Creatinine numeric value distribution", "creatinine_dist")
-plot_hist_numeric_value(numeric_values_cr_cl, "Creatinine clearance numeric value distribution", "creatinine_clearance_dist")
-plot_hist_numeric_value(numeric_values_egfr, "eGFR numeric value distribution", "egfr_dist")
+plot_boxplot_numeric_value(
+    numeric_values_creatinine,
+    "Creatinine numeric value distribution",
+    "creatinine_dist",
+)
+plot_boxplot_numeric_value(
+    numeric_values_cr_cl,
+    "Creatinine clearance numeric value distribution",
+    "creatinine_clearance_dist",
+)
+plot_boxplot_numeric_value(
+    numeric_values_egfr, "eGFR numeric value distribution", "egfr_dist"
+)
