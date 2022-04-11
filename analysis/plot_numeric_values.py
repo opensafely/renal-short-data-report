8c6870a7c850fc0a4de43028ba14d11e1dd16b74
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-04-11 11:39:12 +0100

    add boxplot for numeric values

--- analysis/plot_numeric_values.py
+++ analysis/plot_numeric_values.py
@@ -1,11 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from utilities import (
-    OUTPUT_DIR,
-    match_input_files,
-    get_date_input_file
-)
+from utilities import OUTPUT_DIR, match_input_files, get_date_input_file
 
 numeric_values_creatinine = []
 numeric_values_cr_cl = []
@@ -23,15 +19,27 @@ numeric_values_creatinine = np.concatenate(numeric_values_creatinine)
 numeric_values_cr_cl = np.concatenate(numeric_values_cr_cl)
 numeric_values_egfr = np.concatenate(numeric_values_egfr)
 
+
+def plot_boxplot_numeric_value(x, title, filename):
+    plt.boxplot(x, showfliers=False)
+    plt.title(title)
+    plt.ylabel("count")
+    plt.xlabel("numeric_value")
+    plt.savefig(f"output/{filename}.jpeg")
+    plt.clf()
+
+
 def plot_hist_numeric_value(x, title, filename):
     counts, bins, bars = plt.hist(x, bins=10)
     frequency_counts = [int(x) for x in counts]
-    pd.Series(frequency_counts, name="Counts").to_csv(f'output/{filename}_frequency.csv')
+    pd.Series(frequency_counts, name="Counts").to_csv(
+        f"output/{filename}_frequency.csv"
+    )
     plt.ylabel("count")
     plt.xlabel("numeric_value")
-    plt.savefig(f'output/{filename}.jpeg')
+    plt.savefig(f"output/{filename}.jpeg")
     plt.clf()
-    
+
 
 plot_hist_numeric_value(numeric_values_creatinine, "Creatinine numeric value distribution", "creatinine_dist")
 plot_hist_numeric_value(numeric_values_cr_cl, "Creatinine clearance numeric value distribution", "creatinine_clearance_dist")
