ac8f7e2ade2e1dfced2b46ddaedce258a853438a
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-08-25 10:20:28 +0100

    biochemical ckd vars

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -83,38 +83,6 @@ def create_path_variables(path_tests):
                     "incidence": 0.80,
                 },
             ),
-            f"{test}_ref_range_lower": patients.reference_range_lower_bound_from(
-                "creatinine_numeric_value",
-                return_expectations={
-                    "float": {"distribution": "normal", "mean": 45.0, "stddev": 20},
-                    "incidence": 0.5,
-                },
-            ),
-            f"{test}_ref_range_upper": patients.reference_range_upper_bound_from(
-                "creatinine_numeric_value",
-                return_expectations={
-                    "float": {"distribution": "normal", "mean": 45.0, "stddev": 20},
-                    "incidence": 0.5,
-                },
-            ),
-            f"{test}_numeric_value_oor": patients.categorised_as(
-                {
-                    "above": f"({test}_numeric_value > {test}_ref_range_upper) AND NOT (({test}_operator = '<') OR ({test}_operator = '<=') OR ({test}_operator = '~'))",
-                    "below": f"({test}_numeric_value < {test}_ref_range_lower) AND NOT (({test}_operator = '>') OR ({test}_operator = '>=') OR ({test}_operator = '~'))",
-                    "unknown": f"(({test}_numeric_value > {test}_ref_range_upper) AND (({test}_operator = '<') OR ({test}_operator = '<=') OR ({test}_operator = '~'))) OR (({test}_numeric_value < {test}_ref_range_lower) AND (({test}_operator = '>') OR ({test}_operator = '>=') OR ({test}_operator = '~'))) OR ((({test}_numeric_value > {test}_ref_range_lower) AND ({test}_numeric_value < {test}_ref_range_upper)) AND NOT (({test}_operator = '=') OR ({test}_operator = '')))",
-                    "in range": "DEFAULT",
-                },
-                return_expectations={
-                    "category": {
-                        "ratios": {
-                            "above": 0.2,
-                            "below": 0.2,
-                            "unknown": 0.1,
-                            "in range": 0.5,
-                        }
-                    }
-                },
-            ),
         }
 
     variables = {}
@@ -313,6 +281,234 @@ study = StudyDefinition(
         """
     ),
     **create_path_variables(path_tests),
+    ckd_acr_category=patients.categorised_as(
+        {
+            "None": """
+                acr_numeric_value_history=0
+                """,
+            "A1": """
+                acr_numeric_value_history < 3 AND
+                acr_numeric_value_history > 0 AND
+                NOT acr_operator = ">" AND
+                NOT acr_operator = ">=" AND
+                NOT acr_operator = "<" AND
+                NOT acr_operator = "<="
+                """,
+            "A2": """
+                acr_numeric_value_history > 3 AND
+                acr_numeric_value_history < 30 AND
+                NOT acr_operator = ">" AND
+                NOT acr_operator = ">=" AND
+                NOT acr_operator = "<" AND
+                NOT acr_operator = "<="
+                """,
+            "A3": """
+                acr_numeric_value_history > 30 AND
+                NOT acr_operator = "<" AND
+                NOT acr_operator = "<="
+                """,
+            "Uncategorised": "DEFAULT",
+        },
+        acr_numeric_value_history=patients.with_these_clinical_events(
+            codelist=acr_level_codelist,
+            on_or_before="last_day_of_month(index_date)",
+            returning="numeric_value",
+            date_format="YYYY-MM-DD",
+            include_date_of_match=True,
+            return_expectations={
+                "incidence": 0.5,
+                "date": {"earliest": "1900-01-01", "latest": "today"},
+            },
+        ),
+        acr_numeric_value_history_operator=patients.comparator_from(
+            "acr_numeric_value_history",
+            return_expectations={
+                "rate": "universal",
+                "category": {
+                    "ratios": {  # ~, =, >= , > , < , <=
+                        None: 0.10,
+                        "~": 0.05,
+                        "=": 0.65,
+                        ">=": 0.05,
+                        ">": 0.05,
+                        "<": 0.05,
+                        "<=": 0.05,
+                    }
+                },
+                "incidence": 0.80,
+            },
+        ),
+        return_expectations={
+            "rate": "universal",
+            "category": {
+                "ratios": {
+                    "A1": 0.2,
+                    "A2": 0.2,
+                    "A3": 0.1,
+                    "Uncategorised": 0.1,
+                    "None": 0.4,
+                }
+            },
+        },
+    ),
+    ckd_egfr_category=patients.categorised_as(
+        {
+            "None": """
+                egfr_numeric_value_history=0
+                """,
+            "G1": """
+                egfr_numeric_value_history >= 90 AND
+                egfr_numeric_value_90_before >= 90 AND
+                NOT egfr_numeric_value_history_operator = "<" AND
+                NOT egfr_numeric_value_history_operator = "<=" AND
+                NOT egfr_numeric_value_90_before_operator = "<" AND
+                NOT egfr_numeric_value_90_before_operator = "<="
+                """,
+            "G2": """
+                egfr_numeric_value_history >= 60 AND
+                egfr_numeric_value_history < 90 AND
+                egfr_numeric_value_90_before >= 60 AND
+                egfr_numeric_value_90_before < 90 AND
+                NOT egfr_numeric_value_history_operator = "<" AND
+                NOT egfr_numeric_value_history_operator = "<=" AND
+                NOT egfr_numeric_value_90_before_operator = "<" AND
+                NOT egfr_numeric_value_90_before_operator = "<=" AND
+                NOT egfr_numeric_value_history_operator = ">" AND
+                NOT egfr_numeric_value_history_operator = ">=" AND
+                NOT egfr_numeric_value_90_before_operator = ">" AND
+                NOT egfr_numeric_value_90_before_operator = ">="
+                """,
+            "G3a": """
+                egfr_numeric_value_history >= 45 AND
+                egfr_numeric_value_history < 60 AND
+                egfr_numeric_value_90_before >= 45 AND
+                egfr_numeric_value_90_before < 60 AND
+                NOT egfr_numeric_value_history_operator = "<" AND
+                NOT egfr_numeric_value_history_operator = "<=" AND
+                NOT egfr_numeric_value_90_before_operator = "<" AND
+                NOT egfr_numeric_value_90_before_operator = "<=" AND
+                NOT egfr_numeric_value_history_operator = ">" AND
+                NOT egfr_numeric_value_history_operator = ">=" AND
+                NOT egfr_numeric_value_90_before_operator = ">" AND
+                NOT egfr_numeric_value_90_before_operator = ">="
+                """,
+            "G3b": """
+                egfr_numeric_value_history >= 30 AND
+                egfr_numeric_value_history < 45 AND
+                egfr_numeric_value_90_before >= 30 AND
+                egfr_numeric_value_90_before < 45 AND
+                NOT egfr_numeric_value_history_operator = "<" AND
+                NOT egfr_numeric_value_history_operator = "<=" AND
+                NOT egfr_numeric_value_90_before_operator = "<" AND
+                NOT egfr_numeric_value_90_before_operator = "<=" AND
+                NOT egfr_numeric_value_history_operator = ">" AND
+                NOT egfr_numeric_value_history_operator = ">=" AND
+                NOT egfr_numeric_value_90_before_operator = ">" AND
+                NOT egfr_numeric_value_90_before_operator = ">="
+                """,
+            "G4": """
+                egfr_numeric_value_history >= 15 AND
+                egfr_numeric_value_history < 30 AND
+                egfr_numeric_value_90_before >= 15 AND
+                egfr_numeric_value_90_before < 30 AND
+                NOT egfr_numeric_value_history_operator = "<" AND
+                NOT egfr_numeric_value_history_operator = "<=" AND
+                NOT egfr_numeric_value_90_before_operator = "<" AND
+                NOT egfr_numeric_value_90_before_operator = "<=" AND
+                NOT egfr_numeric_value_history_operator = ">" AND
+                NOT egfr_numeric_value_history_operator = ">=" AND
+                NOT egfr_numeric_value_90_before_operator = ">" AND
+                NOT egfr_numeric_value_90_before_operator = ">="
+                """,
+            "G5": """
+                egfr_numeric_value_history > 0 AND
+                egfr_numeric_value_history < 15 AND
+                egfr_numeric_value_90_before > 0 AND
+                egfr_numeric_value_90_before < 15 AND
+                NOT egfr_numeric_value_history_operator = "<" AND
+                NOT egfr_numeric_value_history_operator = "<=" AND
+                NOT egfr_numeric_value_90_before_operator = "<" AND
+                NOT egfr_numeric_value_90_before_operator = "<=" AND
+                NOT egfr_numeric_value_history_operator = ">" AND
+                NOT egfr_numeric_value_history_operator = ">=" AND
+                NOT egfr_numeric_value_90_before_operator = ">" AND
+                NOT egfr_numeric_value_90_before_operator = ">="
+                """,
+            "Uncategorised": "DEFAULT",
+        },
+        egfr_numeric_value_history=patients.with_these_clinical_events(
+            codelist=eGFR_numeric_value_codelist,
+            on_or_before="last_day_of_month(index_date)",
+            returning="numeric_value",
+            date_format="YYYY-MM-DD",
+            include_date_of_match=True,
+            return_expectations={
+                "incidence": 0.5,
+                "date": {"earliest": "1900-01-01", "latest": "today"},
+            },
+        ),
+        egfr_numeric_value_history_operator=patients.comparator_from(
+            "egfr_numeric_value_history",
+            return_expectations={
+                "rate": "universal",
+                "category": {
+                    "ratios": {  # ~, =, >= , > , < , <=
+                        None: 0.10,
+                        "~": 0.05,
+                        "=": 0.65,
+                        ">=": 0.05,
+                        ">": 0.05,
+                        "<": 0.05,
+                        "<=": 0.05,
+                    }
+                },
+                "incidence": 0.80,
+            },
+        ),
+        egfr_numeric_value_90_before=patients.with_these_clinical_events(
+            codelist=eGFR_numeric_value_codelist,
+            on_or_before="egfr_numeric_value_history_date",
+            returning="numeric_value",
+            date_format="YYYY-MM-DD",
+            include_date_of_match=True,
+            return_expectations={
+                "incidence": 0.5,
+                "date": {"earliest": "1900-01-01", "latest": "today"},
+            },
+        ),
+        egfr_numeric_value_90_before_operator=patients.comparator_from(
+            "egfr_numeric_value_90_before",
+            return_expectations={
+                "rate": "universal",
+                "category": {
+                    "ratios": {  # ~, =, >= , > , < , <=
+                        None: 0.10,
+                        "~": 0.05,
+                        "=": 0.65,
+                        ">=": 0.05,
+                        ">": 0.05,
+                        "<": 0.05,
+                        "<=": 0.05,
+                    }
+                },
+                "incidence": 0.80,
+            },
+        ),
+        return_expectations={
+            "rate": "universal",
+            "category": {
+                "ratios": {
+                    "G1": 0.3,
+                    "G2": 0.1,
+                    "G3a": 0.1,
+                    "G3b": 0.1,
+                    "G4": 0.1,
+                    "Uncategorised": 0.05,
+                    "None": 0.25,
+                }
+            },
+        },
+    ),
     height=patients.with_these_clinical_events(
         height_codelist,
         between=["index_date", "last_day_of_month(index_date)"],
@@ -459,7 +655,11 @@ study = StudyDefinition(
         },
     ),
     latest_renal_date=patients.maximum_of(
-        "dialysis_date", "kidney_tx_date", "RRT_date", "ckd_date", "ckd_primis_1_5_date"
+        "dialysis_date",
+        "kidney_tx_date",
+        "RRT_date",
+        "ckd_date",
+        "ckd_primis_1_5_date",
     ),
     latest_rrt_date=patients.maximum_of("dialysis_date", "kidney_tx_date", "RRT_date"),
     # Picking most recent status
@@ -979,6 +1179,7 @@ study = StudyDefinition(
         },
     ),
 )
+
 
 measures = []
 
