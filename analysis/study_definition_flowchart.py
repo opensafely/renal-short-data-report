90a54048d9a72f0e6c43ef34341309d0fa46c364
Shalini Santhakumaran <98018196+ss808@users.noreply.github.com>
2022-09-15 11:45:32 +0100

    add study def flowchart

---
+++ analysis/study_definition_flowchart.py
@@ -0,0 +1,37 @@
+from cohortextractor import StudyDefinition, patients
+
+study = StudyDefinition(
+    default_expectations={
+        "date": {"earliest": "2019-01-01", "latest": "today"},
+        "rate": "uniform",
+        "incidence": 0.5,
+    },
+    # define study index date
+    index_date="2020-01-01",
+    population=patients.all(),
+#        """
+#            registered AND
+#            NOT died AND
+#            (age >=18 AND age <=120) AND 
+#            (sex = 'M' OR sex = 'F')
+#        """,
+        registered=patients.registered_as_of("index_date"),
+        died=patients.died_from_any_cause(
+            on_or_before="index_date",
+            returning="binary_flag",
+            return_expectations={"incidence": 0.1},
+        ),
+            age=patients.age_as_of(
+        "index_date",
+        return_expectations={
+            "rate": "universal",
+            "int": {"distribution": "population_ages"},
+        },
+    ),
+        sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"M": 0.49, "F": 0.5, "U": 0.01}},
+        }
+    ),
+    )
