8cdad309194ed22420650031224db130632bacae
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-09-15 14:56:46 +0100

    add venn

--- analysis/scripts/crcl_gault.py
+++ analysis/scripts/crcl_gault.py
@@ -3,6 +3,7 @@ import seaborn as sns
 import numpy as np
 import json
 import matplotlib.pyplot as plt
+from venn import venn
 from utilities import OUTPUT_DIR
 
 df = pd.read_csv(OUTPUT_DIR / "input_calculators_calculated.csv.gz", parse_dates = ["creatinine_numeric_value_date", "creatinine_clearance_numeric_value_date"])
@@ -33,4 +34,13 @@ sns.violinplot(data=violin_df, cut=0, inner=None)
 plt.title("CrCl Calculated vs Recorded")
 plt.ylabel("numeric value")
 plt.savefig(OUTPUT_DIR / f"violin_plot_crcl_recorded_vs_calculated.png")
+plt.clf()
+
+
+people_recorded = df.loc[((df["creatinine_clearance_numeric_value"].notnull()) & (df["creatinine_clearance_numeric_value"]> 0)),:]
+people_calculated = df.loc[((df["cg"].notnull()) & (df["cg"]> 0)),:]
+
+venn({"Recorded": set(people_recorded["patient_id"]),
+"Calculated": set(people_calculated["patient_id"])})
+plt.savefig(OUTPUT_DIR / f"venn_crcl_recorded_vs_calculated.png")
 plt.clf()
