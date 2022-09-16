23d0b779de2027de2fa2f006bffdbd68521bdadf
Shalini Santhakumaran <98018196+ss808@users.noreply.github.com>
2022-09-16 13:36:29 +0100

    Add further analysis of incident patients and PC and SC codes (#100)
    * add median age
    
    * add secondary care codes to ukrr study def
    
    * Add PC codes to analysis
    
    * add study def flowchart
    
    * Fix typos
    
    * Amend disclosure control
    
    * fix missing code
    
    * Separate analysis of PC and SC codes for now
    
    * Add other incident variable and analysis
    
    * add incident analysis to project.yaml
    
    * fix errors
    
    * Fix table in rmd output

--- analysis/study_definition_ukrr.py
+++ analysis/study_definition_ukrr.py
@@ -10,6 +10,185 @@ from variable_definitions.incident_variables import incident_variables
 from scripts.variables import demographics
 from codelists import *
 
+def loop_over_proc_codes_prev(code_list):
+
+    def make_variable(code):
+        #removing . from variable name
+        code2=code.replace('.','')
+        return {
+            f"op_procp_{code2}": patients.outpatient_appointment_date(
+                    returning="binary_flag",
+                    with_these_procedures=codelist([code], system="opcs4"),
+                    between=["latest_rrt_date_secondary","latest_rrt_date_secondary"],  #to only pick up codes on the date of interest
+                    return_expectations={
+                        "incidence": 0.3,
+                    },
+                ),
+            f"ip_procp_{code2}": patients.admitted_to_hospital(
+                    returning="binary_flag",
+                    date_format="YYYY-MM-DD",
+                    find_last_match_in_period=True,
+                    with_these_procedures=codelist([code], system="opcs4"),
+                    between=["latest_rrt_date_secondary","latest_rrt_date_secondary"],
+                    return_expectations={
+                        "incidence": 0.3,
+                    },
+                ),
+    #get critical care days (not using _proc_ in name as want to pick this up separately)
+            f"ipp_ccp_{code2}": patients.admitted_to_hospital(
+                    returning="days_in_critical_care",
+                    find_last_match_in_period=True,
+                    with_these_procedures=codelist([code], system="opcs4"),
+                    between=["latest_rrt_date_secondary","latest_rrt_date_secondary"],
+                    return_expectations={
+                        "category": {
+                            "ratios": {
+                                "0": 0.6,
+                                "1": 0.1,
+                                "2": 0.2,
+                                "3": 0.1,
+                                }
+                            },
+                        "incidence": 0.1,
+                        },
+                ),
+        }
+
+    variables = {}
+    for code in code_list:
+        variables.update(make_variable(code))
+    return variables
+
+def loop_over_diag_codes_prev(code_list):
+
+    def make_variable(code):
+        #removing . from variable name
+        code2=code.replace('.','')
+        return {
+            f"ip_diagp_{code2}": patients.admitted_to_hospital(
+                    returning="binary_flag",
+                    find_last_match_in_period=True,
+                    with_these_diagnoses=codelist([code], system="icd10"),
+                    between=["latest_rrt_date_secondary","latest_rrt_date_secondary"],
+                    return_expectations={
+                        "incidence": 0.3,
+                    },
+                ),
+    #get critical care days (not using _diag_ in name as want to pick this up separately)
+            f"ipd_ccp_{code2}": patients.admitted_to_hospital(
+                    returning="days_in_critical_care",
+                    find_last_match_in_period=True,
+                    with_these_diagnoses=codelist([code], system="icd10"),
+                    between=["latest_rrt_date_secondary","latest_rrt_date_secondary"],
+                    return_expectations={
+                        "category": {
+                            "ratios": {
+                                "0": 0.6,
+                                "1": 0.1,
+                                "2": 0.2,
+                                "3": 0.1,
+                                }
+                            },
+                        "incidence": 0.1,
+                        },
+                ),
+        }
+
+    variables = {}
+    for code in code_list:
+        variables.update(make_variable(code))
+    return variables
+
+def loop_over_proc_codes_inc(code_list):
+
+    def make_variable(code):
+        #removing . from variable name
+        code2=code.replace('.','')
+        return {
+            f"op_proci_{code2}": patients.outpatient_appointment_date(
+                    returning="binary_flag",
+                    with_these_procedures=codelist([code], system="opcs4"),
+                    find_first_match_in_period=True,
+                    between=["incident_rrt_date_secondary","incident_rrt_date_secondary"],  #to only pick up codes on the date of interest
+                    return_expectations={
+                        "incidence": 0.3,
+                    },
+                ),
+            f"ip_proci_{code2}": patients.admitted_to_hospital(
+                    returning="binary_flag",
+                    date_format="YYYY-MM-DD",
+                    find_first_match_in_period=True,
+                    with_these_procedures=codelist([code], system="opcs4"),
+                    between=["incident_rrt_date_secondary","incident_rrt_date_secondary"],
+                    return_expectations={
+                        "incidence": 0.3,
+                    },
+                ),
+    #get critical care days (not using _proc_ in name as want to pick this up separately)
+            f"ipp_cci_{code2}": patients.admitted_to_hospital(
+                    returning="days_in_critical_care",
+                    find_first_match_in_period=True,
+                    with_these_procedures=codelist([code], system="opcs4"),
+                    between=["incident_rrt_date_secondary","incident_rrt_date_secondary"],
+                    return_expectations={
+                        "category": {
+                            "ratios": {
+                                "0": 0.6,
+                                "1": 0.1,
+                                "2": 0.2,
+                                "3": 0.1,
+                                }
+                            },
+                        "incidence": 0.1,
+                        },
+                ),
+        }
+
+    variables = {}
+    for code in code_list:
+        variables.update(make_variable(code))
+    return variables
+
+def loop_over_diag_codes_inc(code_list):
+
+    def make_variable(code):
+        #removing . from variable name
+        code2=code.replace('.','')
+        return {
+            f"ip_diagi_{code2}": patients.admitted_to_hospital(
+                    returning="binary_flag",
+                    find_first_match_in_period=True,
+                    with_these_diagnoses=codelist([code], system="icd10"),
+                    between=["incident_rrt_date_secondary","incident_rrt_date_secondary"],
+                    return_expectations={
+                        "incidence": 0.3,
+                    },
+                ),
+    #get critical care days (not using _diag_ in name as want to pick this up separately)
+            f"ipd_cci_{code2}": patients.admitted_to_hospital(
+                    returning="days_in_critical_care",
+                    find_first_match_in_period=True,
+                    with_these_diagnoses=codelist([code], system="icd10"),
+                    between=["incident_rrt_date_secondary","incident_rrt_date_secondary"],
+                    return_expectations={
+                        "category": {
+                            "ratios": {
+                                "0": 0.6,
+                                "1": 0.1,
+                                "2": 0.2,
+                                "3": 0.1,
+                                }
+                            },
+                        "incidence": 0.1,
+                        },
+                ),
+        }
+
+    variables = {}
+    for code in code_list:
+        variables.update(make_variable(code))
+    return variables   
+
 
 study = StudyDefinition(
     default_expectations={
@@ -95,7 +274,46 @@ study = StudyDefinition(
     **rrt_variables,
     **secondary_care_variables,
     **ukrr_variables,
-    **incident_variables
+    **incident_variables,
+
+    #add nearest PC events to UKRR start
+    #primary care
+
+    next_before_allRRT=patients.with_these_clinical_events(
+        codelist=all_rrt_codes,
+        between=["ukrr_inc2020_date - 3 months", "ukrr_inc2020_date"],
+        returning="code",
+        find_last_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        include_date_of_match=True,
+        return_expectations={
+            "category": {"ratios": {"14S2.": 0.5, "7A600": 0.5}},
+            "incidence": 0.8,
+                "date": {"earliest": "2019-10-01", "latest": "2021-03-31"},
+        },
+    ),
+
+    next_after_allRRT=patients.with_these_clinical_events(
+        codelist=all_rrt_codes,
+        between=["ukrr_inc2020_date", "ukrr_inc2020_date + 3 months"],
+        returning="code",
+        find_first_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        include_date_of_match=True,
+        return_expectations={
+            "category": {"ratios": {"14S2.": 0.5, "7A600": 0.5}},
+            "incidence": 0.8,
+                "date": {"earliest": "2019-10-01", "latest": "2021-03-31"},
+        },
+    ),
+
+
+     # creating variables from individual codes, first prevalent
+    **loop_over_proc_codes_prev(RRT_opcs4_codelist),
+    **loop_over_diag_codes_prev(RRT_icd10_codelist),
+         # and now incident
+    **loop_over_proc_codes_inc(RRT_opcs4_codelist),
+    **loop_over_diag_codes_inc(RRT_icd10_codelist),
 )
 
 
