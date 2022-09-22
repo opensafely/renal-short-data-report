3c3042879f748a1d11ea7d415b92f6a9b6fc4800
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-09-22 12:38:00 +0100

    fix column renaming

--- analysis/scripts/join_acr.py
+++ analysis/scripts/join_acr.py
@@ -12,7 +12,14 @@ for file in (OUTPUT_DIR / "joined").iterdir():
         merged_df = df.merge(acr_df, how="left", on="patient_id")
      
         # keep the new acr values.
-        merged_df = merged_df.drop(["acr_x", "acr_code_x", "acr_count_x", "acr_numeric_value_x", "acr_operator_x"], axis=1)
-        merged_df = merged_df.rename({"acr_y": "acr", "acr_code_y": "acr_code", "acr_count_y": "acr_count", "acr_numeric_value_y": "acr_numeric_value", "acr_operator": "acr_operator"})
+        merged_df = merged_df.drop([
+            "acr_x",
+            "acr_date_x"
+            "acr_code_x",
+            "acr_count_x",
+            "acr_numeric_value_x",
+            "acr_operator_x"
+            ], axis=1)
+        merged_df = merged_df.rename(columns={"acr_y": "acr", "acr_date_y": "acr_date","acr_code_y": "acr_code", "acr_count_y": "acr_count", "acr_numeric_value_y": "acr_numeric_value", "acr_operator": "acr_operator"})
 
         merged_df.to_csv(OUTPUT_DIR / "joined" / file.name)
